a7de53619ffc5ee0273fe72c463ae835624bc0b9
Yuval Segal <uvsegal@gmail.com>
2017-07-18 15:02:53 +0200

    first commit

---
+++ app/src/main/java/com/epsilonmoves/inclinationdisplay/dao/RealmQueries.kt
@@ -0,0 +1,37 @@
+package com.epsilonmoves.inclinationdisplay.dao
+
+import io.realm.Realm
+import io.realm.RealmResults
+/**
+* class for the realm DB queries
+* */
+
+class RealmQueries:RealmReadInterface,RealmWriteInterface  {
+    val realm: Realm = Realm.getDefaultInstance()
+
+    override fun insertFacingObjToDb(facing: String, timeStamp: Long, recordId: Int) {
+        val deviceFacing = DeviceFacing()
+        deviceFacing.id = recordId
+        deviceFacing.timeStamp = timeStamp
+        deviceFacing.deviceDirection = facing
+
+        realm.executeTransaction(Realm.Transaction() {
+            realm.copyToRealmOrUpdate(deviceFacing)
+        })
+    }
+
+    override fun allResults(): RealmResults<DeviceFacing>? {
+        return realm.where(DeviceFacing::class.java)
+                .findAllAsync()
+    }
+
+    override fun findNewest(results:RealmResults<DeviceFacing>): String {
+        val max: Long = results.max("timeStamp").toLong()
+
+        val result = realm.where(DeviceFacing::class.java)
+                .equalTo("timeStamp", max)
+                .findFirst()
+        return result.deviceDirection
+    }
+
+}
